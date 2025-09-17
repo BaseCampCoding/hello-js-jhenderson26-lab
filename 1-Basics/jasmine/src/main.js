167ca9a4b1f90cad1a95cf7fb32c462f6519fab9
jhenderson26-lab <jhenderson26@basecampcodingacademy.org>
2025-09-17 10:43:27 -0500

    first done

---
+++ 1-Basics/jasmine/src/main.js
@@ -0,0 +1,11 @@
+const EXPECTED_MINUTES_IN_OVEN = 40;
+
+let actualMinutesInOven = 25;
+
+let remainingMinutesInOven = EXPECTED_MINUTES_IN_OVEN - actualMinutesInOven;
+
+var numberOfLayers = 3;
+
+var preparationTimeInMinutes = 3 * 2;
+
+let totalTimeInMinutes = 25 + preparationTimeInMinutes;
